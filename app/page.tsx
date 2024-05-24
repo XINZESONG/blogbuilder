eae74b844cb0f1234cde36be900d178c622390a3
XINZESONG <otisxzsong@gmail.com>
2024-05-24 13:34:11 +1000

    add about page and compnents

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <main>13d24werwe332</main>;
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <Link href="/about">Link to the About Page</Link>
+    </main>
+  );
 }
